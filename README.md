1ded560b713708fed622759460a267a41373c98f
punasusi <andy@punasusi.com>
2022-01-07 09:32:43 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # rancher-fleet-slack-notifier
+
+## Purpose
+Ranchers Fleet is a really useful gitops tool for syncing a git repostiory with a running kubernetes cluster. Unfortunately, it does not include notifying in case clusters are not in sync. This tool was written to cover such a need. In this case, via a slack channel. It is meant to run in the same cluster where Rancher server is running. 
+
+It will querry the fleet-default cluster list, and generate an alert if any clusters are not in ready state (meaning, either the cluster has an error, or is not in sync). When the cluster is returned to 'ready' state, another message is send indicating that as well. There is a 60 second sleep in between checks.
+
+State of clusters is stored only in memory (sqlite). This means that every restart of this tool will resend any alerts, even if not new and sent already. The database could be moved to an external stateful DB if needed in the future.
+
+<!-- ## Build container image
+If not using container image provided here, an image needs to be built and pushed to an appropriate repository, following is only an exameple.
+
+```
+nerdctl build -t punasusi/slack-notifier:0.0.5 . 
+nerdctl push punasusi/slack-notifier:0.0.5
+```
+
+> nerdctl is part of rancherdesktop and replaces docker command for most everything -->
+
+## Deploy
+First, a namespace is needed. (feel free to change namespace, but update other commands to match)
+```
+kubectl create namespace slack-notifier
+```
+
+Second, a secret needs to be created with the URLs and token. Simple way is from a file. For example:
+a file called secrets.env with content:
+```
+SLACK_NOTIFY_URL=<from slack>
+RANCHER_URL=http://rancher.cattle-system/v1/fleet.cattle.io.clusters/fleet-default
+RANCHER_TOKEN=<bearer token for rancher user>
+```
+could be used, and inserted with the following command
+```
+kubectl --namespace slack-notifier create secret generic slack-creds --from-env-file secrets.env        
+```
+
+Then, apply the kubernetes deployment yaml found in the kubernetes directory
+```
+kubectl --namespace slack-notifier create -f kubernetes/deployment.yaml
+```
+
+
+## TODO
+- [ ] make notification text configurable
+- [ ] better logging
---
+++ app.py
@@ -0,0 +1,92 @@
+import requests
+import json
+import os
+import sqlite3
+import time
+from urllib3.exceptions import InsecureRequestWarning
+
+con = sqlite3.connect(":memory:")
+cur = con.cursor()
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+
+SLACK_NOTIFY_URL = os.getenv('SLACK_NOTIFY_URL')
+RANCHER_URL = os.getenv('RANCHER_URL')
+RANCHER_TOKEN = os.getenv('RANCHER_TOKEN')
+
+headersList = {
+ "Accept": "application/json",
+ "Content-Type": "application/json",
+ "Authorization": f"Bearer {RANCHER_TOKEN}",
+}
+payload = ""
+
+
+def check_rancher_status():
+
+    try:
+        request_response = requests.request("GET", RANCHER_URL, data=payload,  headers=headersList, verify=False)
+    except requests.exceptions.ConnectionError as e:
+        raise SystemExit(f'FATAL ERROR: {e}\n   Cannot connect to rancher URL')
+    try:
+        data = json.loads(request_response._content)
+    except json.decoder.JSONDecodeError as e:
+        raise SystemExit(f'FATAL ERROR: {e}\n  Rancher api data cannot be parsed, check url')
+    try:
+        for i in data["data"]:
+            for j in i['status']['conditions']:
+                if "Ready" in j['type']:
+                    check_exists = 0
+                    cluster_name = i['metadata']['labels']['management.cattle.io/cluster-display-name']
+                    cluster_status = j['status']
+                    cur.execute("SELECT count(*) FROM clusters WHERE name = ?", (cluster_name, ))
+                    check_exists = cur.fetchone()[0]
+                    if check_exists == 0:
+                        cur.execute("INSERT INTO clusters VALUES (?, ?, ?)", (cluster_name, cluster_status, ''))
+                    else:
+                        cur.execute("UPDATE clusters SET ready = ? where name = ?", (f"{cluster_status}", f"{cluster_name}"))
+    except KeyError as e:
+        raise SystemExit(f'FATAL ERROR: {e}\n  Rancher api data cannot be parsed, check token')
+
+
+def send_notif_text(data):
+    slack_data = {'text': data}
+    try:
+        _ = requests.post(
+            SLACK_NOTIFY_URL, data=json.dumps(slack_data),
+            headers={'Content-Type': 'application/json'}
+        )
+    except requests.exceptions as e:
+        raise SystemError(f'ERROR: {e}\n   Cannot send notification')
+
+
+def setup_db():
+    cur.execute('''CREATE TABLE clusters
+                (name text PRIMARY KEY, ready text, notif text)''')
+    con.commit()
+
+
+def check_updates():
+    cur.execute('SELECT name, ready, notif FROM clusters')
+    for row in cur.fetchall():
+        if "True" not in row[1]:
+            if "sync" not in row[2]:
+                notif_string = f":rotating_light: Cluster {row[0]} is not ready, or not in sync with Fleet, please check from Rancher :warning:"
+                send_notif_text(notif_string)
+                cur.execute('UPDATE clusters SET notif = ? where name = ?', (f'{notif_string}', f'{row[0]}'))
+                con.commit()
+        else:
+            cur.execute('UPDATE clusters SET notif = ? where name = ?', ('', f'{row[0]}'))
+            if "rotating_light" in row[2]:
+                notif_string = f"Cluster {row[0]} is in sync. :white_check_mark:"
+                send_notif_text(notif_string)
+                cur.execute('UPDATE clusters SET notif = ? where name = ?', (f'{notif_string}', f'{row[0]}'))
+                con.commit()
+
+
+if __name__ == "__main__":
+    setup_db()
+    while True:
+        check_rancher_status()
+        check_updates()
+        time.sleep(60)
